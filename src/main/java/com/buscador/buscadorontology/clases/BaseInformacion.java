0dd3b785ca14b4dc4a18d97f6fee56e6e71b5908
VioletaBG <viobel998@gmail.com>
2020-12-21 15:55:52 -0400

    Se agrego metodos de limpieza para el ruido

--- src/main/java/com/buscador/buscadorontology/clases/BaseInformacion.java
+++ src/main/java/com/buscador/buscadorontology/clases/BaseInformacion.java
@@ -12,4 +12,77 @@ protected ConsultasBuscador consultaBuscador;
 	  
 	}
 	
+	
+	protected String limpiarNombre(String frase) {
+		   String res = frase;
+		   res = res.replaceAll("-","");
+	    res = res.replaceAll("=","");
+	   
+	    res=res.split("\"")[2];
+	    
+
+		   return res;
+	}
+
+	protected String limpiarDescripcion(String frase) {
+		  String [] separaciones = frase.split("\"");
+		  String res ="";
+		  for(String separacion: separaciones) {
+			  char primerCarac= separacion.charAt(0);
+			  if(primerCarac!='-') {
+				  if(primerCarac!='@') {
+					  if(primerCarac!='^') {
+						  res=res+separacion;
+					  }
+				  }
+				  
+			  }  
+		   }
+		   
+		  return res;
+	}
+
+	protected String limpiarPais(String frase) {
+		   String res ="";
+		   frase= frase.replaceAll("-","");
+	    frase= frase.replaceAll("=","");
+		   String [] separaciones = frase.split("\r\n");
+		   String part3  = separaciones[3];
+		   separaciones = part3.split("\"");
+		   
+		   for(int i=0; i<separaciones.length;i++) {
+			   char primerCarac = separaciones[i].charAt(0);
+			   String palabra = separaciones[i]; 
+			   if(i==1) {
+				  String [] separaciones2 = palabra.split("resource/");
+				 
+			
+				    int j=0;
+				     for(String sepa: separaciones2) {
+				    	 if(j==1){
+				    		res = sepa; 
+				    		res = res.replaceAll("r","");
+				    		res = res.replaceAll("n","");
+				    		res = res.replace("\"\"","");
+				    	 }
+				    	 j++;
+				      
+				     }
+				   
+			   }else if(i==2 && primerCarac!='^') {
+				   res=res+separaciones[i];
+				   
+			   }
+			   
+		   }
+		   
+		   return res;
+	}
+
+	protected String limpiarSimple(String frase) {
+		   String res = frase;
+		   res = res.replaceAll("-","");
+	    res = res.replaceAll("=","");
+	    return res;
+	}
 }
