45f7d2b565b13d516fe7f8c968d337ea65d86995
VioletaBG <viobel998@gmail.com>
2020-12-21 10:51:44 -0400

    objetos clase quesoSMS

--- src/main/java/com/buscador/buscadorontology/clases/BaseQuesoSemiSuave.java
+++ src/main/java/com/buscador/buscadorontology/clases/BaseQuesoSemiSuave.java
@@ -2,18 +2,44 @@ package com.buscador.buscadorontology.clases;
 
 import java.util.ArrayList;
 
+import com.buscador.buscadorontology.ConsultasBuscador;
+
 public class BaseQuesoSemiSuave extends BaseInformacion{
-   private static ArrayList<QuesoSemiSuave> quesosSemiSuaves;
-   
+   private static ArrayList<QuesoSemiSuave> quesosSemiSuaves=new ArrayList<>();
+
+   private  ArrayList<String> nombresIndividuos;
    
    public BaseQuesoSemiSuave() {
-	   quesosSemiSuaves=new ArrayList<>();
-	  
+	  nombresIndividuos = new ArrayList<>();
+	  nombresIndividuos.add("Feta");
+	  nombresIndividuos.add("Fontina");
+	  nombresIndividuos.add("Langres");
+	  nombresIndividuos.add("Livarot");
+	  nombresIndividuos.add("Reblochon");
+	  nombresIndividuos.add("Rocamadour");
+	  nombresIndividuos.add("Tetilla");
+	  nombresIndividuos.add("Waterloo");
+      
+	  añadirBD();
    }
    
    
    public void añadirBD() {
-	
+	   int i=0;
+	   for(String individuo : nombresIndividuos) {
+		   String descrip=consultaBuscador.GetDatoOntologia("descripcionP",individuo);
+		   String pais = consultaBuscador.GetDatoOntologia("paisq", individuo);
+		   String nombre = consultaBuscador.GetDatoOntologia("nombreq", individuo);
+		   String textura = consultaBuscador.GetDatoOntologia("textura", individuo);
+		   String origen = consultaBuscador.GetDatoOntologia("origen", individuo);
+		   String pasteurizado= consultaBuscador.GetDatoOntologia("pasteurizado", individuo);
+		   
+		   QuesoSemiSuave quesoSMS= new QuesoSemiSuave(i+"",descrip,pais, nombre, textura, origen, pasteurizado);
+		   			   
+           quesosSemiSuaves.add(quesoSMS);
+        i++;    
+       }
+	   
    }
    
    public static ArrayList<QuesoSemiSuave> getQuesos(){
