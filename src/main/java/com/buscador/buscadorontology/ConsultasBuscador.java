8b099ad339b24fd847e5aa768f92cd2ff3a2fa40
Edson Rosas <65196797+EdsonRosas3@users.noreply.github.com>
2020-12-21 09:34:19 -0400

    Update ConsultasBuscador.java

--- src/main/java/com/buscador/buscadorontology/ConsultasBuscador.java
+++ src/main/java/com/buscador/buscadorontology/ConsultasBuscador.java
@@ -172,7 +172,21 @@ public class ConsultasBuscador  {
 				 link=ResultSetFormatter.asText(results);
 				 queryExecution.close() ; 
 
-				}	   
+				}
+			if(propiedad=="nombrep"){
+				String prefix="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+							 +"PREFIX owl: <http://www.w3.org/2002/07/owl#>" 
+							 +"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+							 +"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>";
+					String queryNombreP=prefix+"SELECT ?nomP "+
+										"WHERE { ?n <queso:NombrePasta> ?nomP "
+												  +"FILTER(regex(?nomP, "+individuo+"))}";
+													   
+					queryExecution = QueryExecutionFactory.create(queryNombreP, modelo);
+					results = queryExecution.execSelect();
+					link=ResultSetFormatter.asText(results);
+					queryExecution.close() ;													
+			}
 
 		
 		
